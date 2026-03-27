16bccfc0eda949347e0a729dc5bcd48329212337
Octavio Chamblas <octaviochamblasnavarrete@gmail.com>
2026-03-27 09:58:12 -0300

    Implementa vistas CRUD y rutas de cuentas y transacciones

--- gestion/views.py
+++ gestion/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from .models import Cuenta, Transaccion
 
-# Create your views here.
+
+class CuentaListView(ListView):
+    model = Cuenta
+    template_name = 'gestion/cuenta_list.html'
+    context_object_name = 'cuentas'
+
+
+class CuentaDetailView(DetailView):
+    model = Cuenta
+    template_name = 'gestion/cuenta_detail.html'
+    context_object_name = 'cuenta'
+
+
+class CuentaCreateView(CreateView):
+    model = Cuenta
+    template_name = 'gestion/cuenta_form.html'
+    fields = ['cliente', 'numero_cuenta', 'tipo_cuenta', 'saldo', 'activa']
+    success_url = reverse_lazy('cuenta_list')
+
+
+class CuentaUpdateView(UpdateView):
+    model = Cuenta
+    template_name = 'gestion/cuenta_form.html'
+    fields = ['cliente', 'numero_cuenta', 'tipo_cuenta', 'saldo', 'activa']
+    success_url = reverse_lazy('cuenta_list')
+
+
+class CuentaDeleteView(DeleteView):
+    model = Cuenta
+    template_name = 'gestion/cuenta_confirm_delete.html'
+    success_url = reverse_lazy('cuenta_list')
+
+
+class TransaccionListView(ListView):
+    model = Transaccion
+    template_name = 'gestion/transaccion_list.html'
+    context_object_name = 'transacciones'
+
+
+class TransaccionCreateView(CreateView):
+    model = Transaccion
+    template_name = 'gestion/transaccion_form.html'
+    fields = ['cuenta', 'tipo', 'monto', 'descripcion']
+    success_url = reverse_lazy('transaccion_list')
